0eb0142826eb23bee430af43fe648eda96998a70
Tomás Salina <salinatomass53@gmail.com>
2021-10-20 01:20:40 -0300

    useAppContext in the Checkout page

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineDelete } from 'react-icons/ai';
 import '../styles/components/Checkout.css';
 
-import { AiOutlineDelete } from 'react-icons/ai';
+import { useAppContext } from '../context/AppContext';
 
 const Checkout = () => {
+  const { cart, removeFromCart } = useAppContext();
+  const cartHasItems = cart.length > 0;
+
+  const handleRemove = (product) => {
+    removeFromCart(product);
+  };
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de pedidos</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        <h3>{cartHasItems ? 'Lista de pedidos:' : 'Sin pedidos'}</h3>
+        {cart.map((item) => (
+          <div className="Checkout-item">
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+            <button type="button" onClick={() => handleRemove(item)}>
+              <AiOutlineDelete />
+            </button>
           </div>
-          <button type="button">
-            <AiOutlineDelete />
-          </button>
+        ))}
+      </div>
+      {cartHasItems && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio Total: ${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
+            <button type="button">Continuar pedido</button>
+          </Link>
         </div>
-      </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
-        <Link to="/checkout/information">
-          <button type="button">Continuar pedido</button>
-        </Link>
-      </div>
+      )}
     </div>
   );
 };
