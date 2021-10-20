9e76bfb67cc5a0228231de3d57f298dd21143bb1
Tomás Salina <salinatomass53@gmail.com>
2021-10-19 22:24:39 -0300

    Checkout layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../styles/components/Header.css';
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">PlatziConfMerch</h1>
+      <h1 className="Header-title">Platzi Conf Merch</h1>
       <div className="Header-checkout">Checkout</div>
     </div>
   );
